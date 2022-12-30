33a89d775a42bdb20d6befc5fec21e9965cb1d9f
Wellington Bernarr <wellingt@student.21-school.ru>
2022-12-30 17:20:36 +0300

    bugfixes: -- add to git files

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-import express from 'express'
+const express = require('express')
 
 const app = express()
-const PORT = 8888
+const PORT = 3000
 
 app.use(express.static('./dist/'))
+
+app.get("/*", function (req, res) {
+    res.sendFile("index.html", { root: "./dist"});
+});
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`))
